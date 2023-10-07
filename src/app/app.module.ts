339b135812ff637998c88126d1bd8d49b2b63aba
kokilapatelus <kokilapatelus@gmail.com>
2023-10-07 19:32:14 -0400

    Form Componant Changes Submitted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    SellerAuthComponent
+    SellerAuthComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -11,5 +11,8 @@ export class SellerAuthComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  signUp(data:object): void {
+    console.warn(data)
+  }
 
 }
